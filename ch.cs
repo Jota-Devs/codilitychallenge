e8aab915824efa7b42b53cac5b13aefe1911bbbb
Juan Garcia da Rosa <garciadarosajuan@gmail.com>
2021-02-24 11:30:10 -0300

    first commit

---
+++ ch.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+class Solution {
+    public int solution(int[] A) {
+        Array.Sort(A);     
+        int ret = 1; 
+        if(A[A.Length-1] < 1) return ret;
+        if(A[0]>1) return ret;
+        for (int i = 0; i < A.Length; i++){
+            if(A[i] == ret){
+                ret++;
+            }
+        }   
+        return ret;    
+    }
+}
+
